3f5bca8ded4b6e4768cc7cdf84fada6010b707fd
roshinimareedu <roshinimareedu11@gmail.com>
2025-07-27 16:33:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import DynamicForm from "./components/DynamicForm";
-
-const API_URL =
-  "https://v2-dev-api.esigns.io/v1.0/templates-v2/687893c84c8b5bf090eacc09?company_id=678a07e588cd760d7bd74a6c";
+import React, { useState } from "react";
+import DynamicForm from "./DynamicForm";
 
 function App() {
+  const [apiUrl, setApiUrl] = useState(
+    "https://v2-dev-api.esigns.io/v1.0/templates-v2/687893c84c8b5bf090eacc09?company_id=678a07e588cd760d7bd74a6c"
+  );
+
   return (
-    <div className="App">
+    <div>
       <h2>Dynamic Form Generator</h2>
-      <DynamicForm apiUrl={API_URL} />
+      <DynamicForm apiUrl={apiUrl} />
     </div>
   );
 }
 
 export default App;
+
